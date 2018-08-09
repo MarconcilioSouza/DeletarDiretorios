dd890a09ba99945c9cbf8689a57f79019fe32624
marconcilio <marconcilio@hotmail.com>
2018-08-08 21:45:42 -0300

    deletardiretorios

---
+++ deletardiretorios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deletardiretorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deletarDiretorios(@"C:\WORK\1-Arquivos Pessoais\Copias");
+        }
+
+
+        private static void deletarDiretorios(string local)
+        {
+            string[] fileArray = Directory.GetDirectories(local);
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+                if (fileArray[i].Contains(@"\Package") || fileArray[i].Contains(@"\bin") || fileArray[i].Contains(@"\obj"))
+                {
+                    Console.WriteLine(fileArray[i]);
+                    System.IO.DirectoryInfo di = new DirectoryInfo(fileArray[i].ToString());
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+                }
+                else
+                    deletarDiretorios(fileArray[i].ToString());
+            }
+        }
+    }
+}
